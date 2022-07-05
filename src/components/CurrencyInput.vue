ea3e09643518362559ad78af98d8556023682fa0
Nick Yurin <yurinnik@gmail.com>
2022-07-05 16:56:46 +0300

    change arrow style, removed one console.log() i missed

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -25,7 +25,6 @@ export default {
       this.$emit("changeAmount", this.amount);
     },
     currencyChangeHandler() {
-      console.log("change currency");
       this.$emit("changeCurrency", this.currency);
     },
   },
@@ -50,20 +49,41 @@ select {
   @media screen and (max-width: 400px) {
     font-size: 1rem;
   }
-  padding: 10px 0px 10px 15px;
+  padding: 10px 20px 10px 15px;
   background: none;
   border: 0;
   color: $main-color;
+  &:focus {
+    outline: none;
+  }
 }
 
+select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
 select::-webkit-scrollbar {
   display: none;
 }
 
-input:focus,
-select:focus {
-  outline: none;
+.currency {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &::after {
+    content: "";
+    position: relative;
+    right: 20px;
+    width: 0.8em;
+    height: 0.5em;
+    background-color: $main-color;
+    clip-path: polygon(100% 0%, 0 0%, 50% 100%);
+  }
 }
+
+
 /* Chrome, Safari, Edge, Opera */
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
